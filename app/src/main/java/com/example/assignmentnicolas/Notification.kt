7a222ddeff024677fea13377b73321f976c66948
Nicolas Al Khoury <nicolas.n.khoury@gmail.com>
2021-11-14 16:48:40 +0200

    notification

---
+++ app/src/main/java/com/example/assignmentnicolas/Notification.kt
@@ -0,0 +1,46 @@
+package com.example.assignmentnicolas
+
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class Notification: FirebaseMessagingService()
+{
+
+    companion object
+    {
+        @JvmStatic
+        val CHANNEL_ID = "1"
+    }
+
+    override fun onNewToken(token: String)
+    {
+        super.onNewToken(token);
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage)
+    {
+        super.onMessageReceived(remoteMessage)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.index)
+            .setContentTitle(remoteMessage.notification!!.title)
+            .setContentText(remoteMessage.notification!!.body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this))
+        {
+            notify(3, builder.build())
+        }
+
+        if (remoteMessage.notification!!.title == "camera")
+        {
+            val intent = Intent(this, camera::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+            startActivity(intent)
+        }
+
+    }
+}
